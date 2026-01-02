931c0a910bcf46357030d00bd8d827eeb983e042
Prashant Joshi <prashantjoshi087@gmail.com>
2026-01-02 21:43:07 +0530

    added city in student model

--- studenthub/models.py
+++ studenthub/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     mobile_number = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return self.name
+    
+
 
